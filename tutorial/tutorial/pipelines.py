d67a7492195b4ec9e5a552a67589178ea877d549
Godning <736397792@qq.com>
2017-01-12 01:07:23 +0800

    update pipelines

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -5,9 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 #from twisted.enterprise import adbapi
+from scrapy.exceptions import DropItem
 import pymysql
 import pymysql.cursors
-
+import redis
+Redis = redis.StrictRedis(host='localhost',port=6379,db=0)
 
 class TutorialPipeline(object):
     def __init__(self):
@@ -25,6 +27,15 @@ class TutorialPipeline(object):
         self.connection.commit()
         return item
 
+
+class DuplicatesPipeline(object):
+    def process_item(self, item, spider):
+        if Redis.exists('url:%s' % item['link']):
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            Redis.set('url:%s' % item['link'],1)
+            return item
+
 class SohuPipeline(object):
     def __init__(self):
         self.connection = pymysql.connect(
@@ -35,15 +46,17 @@ class SohuPipeline(object):
             passwd='',
 			charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor,)
-        self.titles_seen = set()
 
     def process_item(self, item, spider):
-        if item["title"][0] not in self.titles_seen:
-            sql = "insert into news values(%s, %s)"
+        if True:
+            sql = "insert into news values(%s, %s, %s)"
             content = ""
+            link = ""
             for constr in item["content"]:
                 content += constr
-                self.connection.cursor().execute(sql, (item["title"][0], content))
+                self.connection.cursor().execute(sql, (item["title"][0], content,item["link"]))
                 self.connection.commit()
-            self.titles_seen.add(item["title"][0])
             return item
+
+    def close_spider(self,spider):
+        self.connection.close()
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -66,6 +66,7 @@ ROBOTSTXT_OBEY = True
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'tutorial.pipelines.SohuPipeline': 300,
+    'tutorial.pipelines.DuplicatesPipeline': 200,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
